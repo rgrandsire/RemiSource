001d198df192a3c70794181bebc0952eb58e2a96
rgrandsire <rgrandsire@maintenanceconnection.com>
2016-11-15 16:31:34 -0500

    Get rid of ConfigurationSettings and replace with Configuration
    Added one DLL as a reference

--- Sammamish/SammamishImport/ConsoleApplication1/Program.cs
+++ Sammamish/SammamishImport/ConsoleApplication1/Program.cs
@@ -13,13 +13,8 @@
 */
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Data.SqlClient;
-using System.Configuration;
 
 namespace SammamishMeterImport
 {
